282668fe25828938261160c2ba1427357b231914
romanB <romanbactol@gmail.com>
2021-12-01 13:59:21 -0600

    Put js in mapboxTest.js

--- static/mapboxTest.js
+++ static/mapboxTest.js
@@ -1,15 +1,40 @@
 
+
+
+
+
+
 //navigator.geolocation.getCurrentPosition(position => {
 //const { longitude, latitude } = position.coords;
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiaGF5bGV5aGFkZ2VzIiwiYSI6ImNrdzB5M2docTd2eXkzMXMxcHAxcXV1NmMifQ.RpqjqIW44lhJYhcCr8fvIg';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic21jbmFsbDEiLCJhIjoiY2t3bW1wcHh1MmRocDJ0bm9pamdvaHA2eCJ9.Z5gTQQvyzRvEd-xhExmtiA';
 const stPaul = [-93.0900, 44.9537]
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/hayleyhadges/ckw1009py0qw314qtoha4yzun', // style URL
+    style: 'mapbox://styles/smcnall1/ckwmrgftc1ey915obmyxl17er', // style URL
     center: stPaul,
     // Mapbox GL JS uses [longitude, latitude] format
     zoom: 12
+});
+
+map.on('load', () => {
+    map.addSource('parks', {
+        type: 'geojson',
+        // Use a URL for the value for the `data` property.
+        data: 'https://api.mapbox.com/datasets/v1/smcnall1/ckwmqij727ye627mnh03nfal8/features?access_token=pk.eyJ1Ijoic21jbmFsbDEiLCJhIjoiY2t3bW1wcHh1MmRocDJ0bm9pamdvaHA2eCJ9.Z5gTQQvyzRvEd-xhExmtiA'
+    });
+
+    map.addLayer({
+        'id': 'national-parks-layer',
+        'type': 'circle',
+        'source': 'parks',
+        'paint': {
+            'circle-radius': 8,
+            'circle-stroke-width': 2,
+            'circle-color': 'red',
+            'circle-stroke-color': 'white'
+        }
+    });
 });
 //});
 
@@ -21,19 +46,6 @@ const popup = new mapboxgl.Popup({ offset: 25 }).setText(
 // create DOM element for the marker
 const el = document.createElement('div');
 el.id = 'marker';
-
-
-const marker = new mapboxgl.Marker({
-    color: "#000000",
-    draggable: false
-}).setLngLat([-93.0900, 44.9537]).setPopup(popup).addTo(map);
-
-
-// create the marker
-// new mapboxgl.Marker(el)
-// .setLngLat(stPaul)
-// .setPopup(popup) // sets a popup on this marker
-// .addTo(map);
 
 
 function openForm() {
