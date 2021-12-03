8422818362b302152b7aad7ad56bdc72e35beb91
hayleyhadges <hhadges@macalester.edu>
2021-12-03 11:05:25 -0600

    commit to pull

--- hayley/app.py
+++ hayley/app.py
@@ -21,22 +21,28 @@ def map_page():
         location_rating = request.form.get("location_rating")
         location_review = request.form.get("location_review")
         db.create("1234",location_review, location_rating)
+    
+    reviewItems = db.get()
+
+    return render_template("mapPage.html", mapbox_access_token=mapbox_access_token, reviewItems = reviewItems)
 
 
+    # Original app.py
+
+from flask import Flask, request, jsonify
+from flask.templating import render_template
+
+app = Flask(__name__)
+app.config.from_pyfile('server.cfg')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+@app.route("/")
+def homepage():
+    return render_template("test.html")
+
+@app.route("/map", methods=['GET', 'POST'])
+def map_page():
+
+    mapbox_access_token = 'pk.eyJ1Ijoic21jbmFsbDEiLCJhIjoiY2t3bW1wcHh1MmRocDJ0bm9pamdvaHA2eCJ9.Z5gTQQvyzRvEd-xhExmtiA'
+    
     return render_template("mapPage.html", mapbox_access_token=mapbox_access_token)
-
-
-
-
-
-    
-@app.route('/',methods=['GET', 'POST'])
-def show_all_reviews():
-  if request.method == 'POST':
-    movie_name = request.form.get("movie_name")
-    movie_review = request.form.get("movie_review")
-    movie_rating = request.form.get("movie_rating")
-    
-    db.create(location_review, movie_rating)
-  reviewItems = db.get()
-  return render_template("home.html", reviewItems = reviewItems)
